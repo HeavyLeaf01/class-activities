d1edd2e762c6024970c75aea394c011169149dad
Gabriel Fernandes Temp0Ni <0114882@ifmg.edu.br>
2025-10-24 10:58:06 -0300

    modified:   DNApice.py

--- DNApice.py
+++ DNApice.py
@@ -1,14 +1,41 @@
-ParesBacteria = input("Digite a quantidade de pares que a bacteria possui: ")
-ParesAParecendo = input("Digite quantos pares você quer que apareça: ")
+from random import *
 
-bases{'A',
-      'C',
-      'T',
-      'G',
-      'U'}
-def geraDNA(quantidade,tamanho, base):
-    for k in range(0, 4):
-        base1 = base[k]
-        for i in range (0, quantidade):
-        if base == 'A':
+QtdPares = int(input("Quantos pares de bases você deseja?: "))
+QtdParesTela = int(input("de quantos em quantos pares você deseja que apareça na tela: "))
+bases = ['A', 'C', 'T', 'G']
+def defbase(QtdPares):
+    fita1=[]
+    fita2=[]
+    
+    for k in range(QtdPares):
+        fita1.append(bases[randint(0,3)])
+
+        base1 = fita1[k]
+        if base1 == "A":
+            base2 = "T"
+        elif base1 == "C":
+            base2 = "G"
+        elif base1 == "T":
+            base2 = "A"
+        elif base1 == "G":
+            base2 = "C"
+
+        fita2 +=[base2]
+    return (fita1, fita2)
+
+cores = {'A' : "\033[0;31mA",
+         'T' : "\033[0;32mT",
+         'G' : "\033[0;35mG",
+         'C' : "\033[0;37mC",
+         '-' : "\033[0;36m-",
+         '|' : "\033[0;36m|"}
+fita1, fita2 = defbase(QtdPares)
+
+for j in range(0,QtdPares):
+    print(cores['|'],cores[fita1[j]],cores['-'],cores['-'],cores[fita2[j]],cores['|'])
+
+print("\033[0m")
+
+#RNA
+
 
